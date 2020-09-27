5a22cec342ffded91a96002a2118e6dc90283966
kokkokeko <kk5321384@gmail.com>
2020-09-27 14:21:35 +0900

    Create game view, html and css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,54 @@
-import React from 'react';
+import React from 'react'
+import './App.css'
+
+function ListItem(props) {
+  return <li className="card frontCard">{props.value}</li>
+}
+
+function CardList(props) {
+  const listItems = props.nums.map( num => {
+    return <ListItem key={num.toString()} value={num} />
+  })
+
+  return (
+    <ul className="wrapper">
+      {listItems}
+    </ul>
+  )
+}
+
+function App() {
+  const cardNumbers = [1,2,3,4,5,1,2,3,4,5]
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <div className="cards">
+        <CardList nums={cardNumbers}/>
+{/*     
+<ul className="wrapper">
+    <li className="card backCard">{cardNumbers[0]}</li>
+        <li className="card frontCard">{cardNumbers[1]}</li>
+        <li className="card">{cardNumbers[2]}</li>
+        <li className="card">{cardNumbers[3]}</li>
+        <li className="card">{cardNumbers[4]}</li>
+        <li className="card">{cardNumbers[5]}</li>
+        <li className="card">{cardNumbers[6]}</li>
+        <li className="card">{cardNumbers[7]}</li>
+        <li className="card">{cardNumbers[8]}</li>
+        <li className="card">{cardNumbers[9]}</li> 
+                </ul>
+                */}
+
+      </div>
+    </div>
+  )
+}
+
+const classList = ["card", "frontCard", "backCard", "removedCard"]
+export default App
+
+
+/* import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +58,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          dit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -24,3 +74,4 @@ function App() {
 }
 
 export default App;
+ */
